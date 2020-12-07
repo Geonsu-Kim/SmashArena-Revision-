02f22b0ec0d67a5395823bbc4ac2d8766a618e75
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2020-12-07 22:09:00 +0900

    UI매니저 통합

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager :SingletonBase<UIManager>
+{
+    public GameObject EnemyInfo;
+    public Image EnemyHpBar;
+    public Text EnemyName;
+
+    public Image PlayerHpBar;
+    public Image PlayerMpBar;
+
+    public void RenewPlayerUI(ref Image image,float ratio)
+    {
+        image.fillAmount = ratio;
+    }
+    public void RenewEnemyUI(ref Color color,ref string name,float ratio)
+    {
+        if (!EnemyInfo.activeSelf) EnemyInfo.SetActive(true);
+        EnemyName.text = name;
+        EnemyHpBar.fillAmount = ratio;
+    }
+}
