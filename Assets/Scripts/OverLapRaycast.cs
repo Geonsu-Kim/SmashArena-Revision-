99ba31e279014abc490a54d7e10846d25afa1d66
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-01-14 17:43:20 +0900

    히트박스 플레이어 적 구분없이 공용으로 사용가능

---
+++ Assets/Scripts/OverLapRaycast.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class OverLapRaycast
+{   
+    public static Collider[] CheckSphere(float radius,Vector3 position,int layerMask=1<<10)
+    {
+        return Physics.OverlapSphere(position, radius, layerMask);
+    }
+
+    public static Collider[] CheckBox(Vector3 size, Vector3 position,Quaternion dir, int layerMask = 1 << 10)
+    {
+        return Physics.OverlapBox(position, size, dir, layerMask);
+    }
+}
