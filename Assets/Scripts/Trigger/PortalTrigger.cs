d5622bd7d3ff2d5dd097cfd99d4e434c0b4e4cb5
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-01-14 17:54:44 +0900

    포탈 트리거

---
+++ Assets/Scripts/Trigger/PortalTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PortalTrigger : MonoBehaviour
+{
+    private FSMPlayer player;
+    public Vector3 WarpPos;
+    public string nextScene;
+    public string _text;
+    AsyncOperation op;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject.GetComponent<FSMPlayer>();
+            player.BtnNum = 2;
+            player.portal = this;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject.GetComponent<FSMPlayer>();
+            player.BtnNum = 0;
+            player.portal = null;
+        }
+    }
+    public void MoveToNextScene()
+    {
+        player.StartPos = WarpPos;
+        Time.timeScale = 0;
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        op=SceneManager.LoadSceneAsync(nextScene,LoadSceneMode.Additive);
+    }
+}
