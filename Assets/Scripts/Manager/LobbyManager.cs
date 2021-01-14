e0f544a1b16e4e4eab19a3d48e13f1c7b47d63c1
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-01-14 17:53:06 +0900

    게임 로비 매니저
    인스턴스화 시 던전 정보가 저장된 XML로부터 정보를 불러와 던전 목록에 추가함

---
+++ Assets/Scripts/Manager/LobbyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : SingletonBase<LobbyManager>
+{
+    // Start is called before the first frame update
+    public GameObject DungeonListView;
+    public Dungeon[] dungeons;
+    private void Awake()
+    {
+        dungeons = DungeonListView.GetComponentsInChildren<Dungeon>();
+        DungeonDataIO.LoadData();
+    }
+
+}
