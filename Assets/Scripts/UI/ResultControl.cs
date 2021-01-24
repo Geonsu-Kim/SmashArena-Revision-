02ee7708f598a6acb147ef2916b4390ba38c330e
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-01-24 21:26:28 +0900

    결과창 추가

---
+++ Assets/Scripts/UI/ResultControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ResultControl : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI redGemText;
+    public TextMeshProUGUI expText;
+    public TextMeshProUGUI scoreText;
+
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        int m = (int)(GameSceneManager.Instance.playTime / 60);
+        int s= (int)(GameSceneManager.Instance.playTime % 60);
+        timeText.text = m.ToString() + " : " + s.ToString();
+        redGemText.text = PlayerManager.Instance.Player.redGem.ToString();
+        expText.text = PlayerManager.Instance.Player.Exp.ToString();
+        scoreText.text = PlayerManager.Instance.score.ToString();
+
+    }
+    public void GotoLobby()
+    {
+        PlayerDataIO.SaveData();
+        LoadingSceneManager.LoadScene("scLobby");
+    }
+    public void Retry()
+    {
+
+        PlayerDataIO.SaveData();
+        LoadingSceneManager.LoadScene(GameSceneManager.Instance.dungeonName);
+    }
+}
