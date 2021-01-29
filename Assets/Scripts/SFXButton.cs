271aec7441f4fa256645528ec3be1d162e40e2af
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-01-29 09:04:55 +0900

    기본 버튼을 상속받은 효과음 버튼

---
+++ Assets/Scripts/SFXButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SFXButton : Button
+{
+    private const string buttonClick = "ButtonClick";
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        if (SoundManager.Instance!=null)
+        {
+            onClick.AddListener(ClickSound);
+        }
+    }
+    void ClickSound()
+    {
+        SoundManager.Instance.PlaySFX(buttonClick);
+    }
+
+}
